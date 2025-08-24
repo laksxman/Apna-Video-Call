d6bfc1c769409460ab1085231f028595c89a96e4
Lakshman Kumar <kumarlaksxman17@gmail.com>
2025-08-24 16:30:08 +0530

    fix error

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -7,7 +7,7 @@ let timeOnline = {}
 export const connectToSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "*",
+            origin: "https://zoomvideocallfrontend-jc9j.onrender.com",
             methods: ["GET", "POST"],
             allowedHeaders: ["*"],
             credentials: true
