d80eda6a9914e6d3204bbab0218a564bd73ed7d5
Lakshman Kumar <kumarlaksxman17@gmail.com>
2025-08-24 15:45:53 +0530

    Replace bcrypt with bcryptjs for cloud deploy

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import httpStatus from "http-status";
 import { User } from "../models/user.model.js";
-import bcrypt, { hash } from "bcrypt"
+import bcrypt, { hash } from "bcryptjs";
 
-import crypto from "crypto"
+import crypto from "crypto";
 import { Meeting } from "../models/meeting.model.js";
 
 const login = async (req, res) => {
